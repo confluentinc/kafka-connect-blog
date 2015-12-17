2cb93742687c3a7a9934c2f04c9ab3282b13454b
Liquan Pei <liquanpei@gmail.com>
2015-12-16 18:29:38 -0800

    Add sleep between service starts

--- start.sh
+++ start.sh
@@ -10,9 +10,13 @@ hadoop-daemon.sh start datanode
 # Starting Confluent Platform
 echo "Starting Confluent Platform"
 zookeeper-server-start /mnt/etc/zookeeper.properties 1>> /mnt/logs/zk.log 2>>/mnt/logs/zk.log &
-kafka-server-start /mnt/etc/server.properties 1>> /mnt/logs/kafka.log 2>> /mnt/logs/kafka.log &
-schema-registry-start /mnt/etc/schema-registry.properties 1>> /mnt/logs/schema-registry.log 2>> /mnt/logs/schema-registry.log &
+sleep 5
 
+kafka-server-start /mnt/etc/server.properties 1>> /mnt/logs/kafka.log 2>> /mnt/logs/kafka.log &
+sleep 5
+
+schema-registry-start /mnt/etc/schema-registry.properties 1>> /mnt/logs/schema-registry.log 2>> /mnt/logs/schema-registry.log &
+sleep 5
 # Starting Hive Metastore
 echo "Starting Hive metastore"
 hive --service metastore 1>> /mnt/logs/metastore.log 2>> /mnt/logs/metastore.log &
